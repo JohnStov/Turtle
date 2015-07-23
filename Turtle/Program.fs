a4fbed83c5b8b4dafd0477185bd32df3d8a76724
John Stovin <john.d.stovin@gmail.com>
2015-07-23 13:04:05 +0100

    Draw line on canvas

--- Turtle/Program.fs
+++ Turtle/Program.fs
@@ -1,5 +1,11 @@
 ﻿module Program
 
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Shapes
+open System.Windows.Media
+
 let assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name
 
 let getXamlResource xaml = 
@@ -8,7 +14,19 @@ let getXamlResource xaml =
   |> System.Windows.Application.LoadComponent
   :?> _
 
-let mainWindow : System.Windows.Window = getXamlResource "MainWindow.xaml"
+let mainWindow : Window = getXamlResource "MainWindow.xaml" 
+let canvas = mainWindow.FindName("canvas") :?> Canvas
+
+let drawLine (canvas : Canvas) X1 Y1 X2 Y2 =
+    let line = new Line()
+    line.Stroke <- Brushes.Red
+    line.X1 <- X1
+    line.X2 <- X2
+    line.Y1 <- Y1
+    line.Y2 <- Y2
+    canvas.Children.Add(line) |> ignore
+
+drawLine canvas 100.0 100.0 200.0 200.0
 
 [<System.STAThread>]
 do System.Windows.Application().Run(mainWindow) |> ignore
