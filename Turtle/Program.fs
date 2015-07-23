70be473ab5b5da3dab726af33e305ef32265cf71
John Stovin <john.d.stovin@gmail.com>
2015-07-23 13:10:19 +0100

    Quick refactor

--- Turtle/Program.fs
+++ Turtle/Program.fs
@@ -1,23 +1,22 @@
 ﻿module Program
 
 open System
+open System.Reflection
 open System.Windows
 open System.Windows.Controls
 open System.Windows.Shapes
 open System.Windows.Media
 
-let assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name
-
 let getXamlResource xaml = 
-  sprintf "/%s;component/%s" assemblyName xaml
-  |> fun nme -> System.Uri(nme, System.UriKind.Relative)
-  |> System.Windows.Application.LoadComponent
+  sprintf "/%s;component/%s" (Assembly.GetExecutingAssembly().GetName().Name) xaml
+  |> fun nme -> Uri(nme, UriKind.Relative)
+  |> Application.LoadComponent
   :?> _
 
 let mainWindow : Window = getXamlResource "MainWindow.xaml" 
 let canvas = mainWindow.FindName("canvas") :?> Canvas
 
-let drawLine (canvas : Canvas) X1 Y1 X2 Y2 =
+let drawLine X1 Y1 X2 Y2 =
     let line = new Line()
     line.Stroke <- Brushes.Red
     line.X1 <- X1
@@ -26,7 +25,7 @@ let drawLine (canvas : Canvas) X1 Y1 X2 Y2 =
     line.Y2 <- Y2
     canvas.Children.Add(line) |> ignore
 
-drawLine canvas 100.0 100.0 200.0 200.0
+drawLine 100.0 100.0 200.0 200.0
 
 [<System.STAThread>]
 do System.Windows.Application().Run(mainWindow) |> ignore
