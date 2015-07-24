eb29bbc51f9c2a8a618e07c055d7fa9a3bbb5ad4
John Stovin <john.d.stovin@gmail.com>
2015-07-24 12:44:22 +0100

    Multiple lines

--- Turtle/Program.fs
+++ Turtle/Program.fs
@@ -7,6 +7,10 @@ open System.Windows.Controls
 open System.Windows.Shapes
 open System.Windows.Media
 
+
+type MyLine = { x1 : float; x2 : float; y1 : float; y2 : float
+}
+
 let getXamlResource xaml = 
   sprintf "/%s;component/%s" (Assembly.GetExecutingAssembly().GetName().Name) xaml
   |> fun nme -> Uri(nme, UriKind.Relative)
@@ -16,16 +20,24 @@ let getXamlResource xaml =
 let mainWindow : Window = getXamlResource "MainWindow.xaml" 
 let canvas = mainWindow.FindName("canvas") :?> Canvas
 
-let drawLine X1 Y1 X2 Y2 =
+let drawLine myLine =
     let line = new Line()
     line.Stroke <- Brushes.Red
-    line.X1 <- X1
-    line.X2 <- X2
-    line.Y1 <- Y1
-    line.Y2 <- Y2
+    line.X1 <- myLine.x1
+    line.X2 <- myLine.x2
+    line.Y1 <- myLine.y1
+    line.Y2 <- myLine.y2
     canvas.Children.Add(line) |> ignore
 
-drawLine 100.0 100.0 200.0 200.0
+let rec drawLines lines =
+    match lines with
+    | [] -> ignore
+    | h :: t -> 
+        drawLine h
+        drawLines t
+
+let myLines = [{ x1 = 100.0; y1 = 100.0; x2 = 200.0; y2 = 200.0; }; { x1 = 200.0; y1 = 200.0; x2 = 300.0; y2 = 100.0; }]
+drawLines myLines |> ignore
 
 [<System.STAThread>]
 do System.Windows.Application().Run(mainWindow) |> ignore
